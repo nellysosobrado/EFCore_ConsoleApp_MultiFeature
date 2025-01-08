20d2604193613a709f6460769553ccbcc94011f1
Nelly <Nelly.sosobrado@gmail.com>
2025-01-08 10:55:18 +0100

    Created new project 'Startup', with basic test Menu using Spectre.Console package

---
+++ StartUp/Program.cs
@@ -0,0 +1,44 @@
+﻿using Spectre.Console;
+
+namespace StartUp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ShowMenu();
+            }
+        }
+
+        private static void ShowMenu()
+        {
+            Console.Clear();
+
+            var option = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("[green]Choose an option:[/]")
+                    .WrapAround(true)
+                    .AddChoices(new[] { "Start Calculator", "Exit" }));
+
+            switch (option)
+            {
+                case "Start Calculator":
+                    break;
+
+
+                case "Exit":
+                    AnsiConsole.MarkupLine("[red]Exiting the application...[/]");
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    AnsiConsole.MarkupLine("[yellow]Invalid option. Try again![/]");
+                    break;
+            }
+        }
+
+      
+    }
+}
