9737879ba463799e90cebbf83b414ce3ff33eee4
Nelly <Nelly.sosobrado@gmail.com>
2025-01-08 10:38:34 +0100

    Set Up Migrations and Database Context. And testing if it works.
    - Created a Migrations folder to store migration files. And Added InitalCreate file
    
    - Added a DesignTimeDbContextFactory class to provide an instance of the database context during runtime.

---
+++ ClassLibrary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ClassLibrary.Data;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Checking database...");
+
+        var dbContext = DatabaseConfig.GetDbContext();
+
+        dbContext.Database.EnsureCreated();
+
+        Console.WriteLine("Creatwed and connected!");
+    }
+}
