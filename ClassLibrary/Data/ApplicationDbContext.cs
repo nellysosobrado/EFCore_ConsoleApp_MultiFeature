2b0e33d8f697315b9c1c962818156976f69f4379
Nelly <Nelly.sosobrado@gmail.com>
2025-01-08 08:37:02 +0100

    - Created: ApplicationDbContext.cs and DatabaseConfig.cs for setting upp database & to manage database connections.
    - Updated ClassLibrary.csproj to include new packages for configuration.

---
+++ ClassLibrary/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Models.Calculator> Calculations { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ClassLibrary/Data/DatabaseConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace ClassLibrary.Data
+{
+    public class DatabaseConfig
+    {
+        public static ApplicationDbContext GetDbContext()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            var config = builder.Build();
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseSqlServer(config.GetConnectionString("DefaultConnection"))
+                .Options;
+
+            return new ApplicationDbContext(options);
+        }
+    }
+}
