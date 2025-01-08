612538778e36a038a9f864e2a4f11aef8ca085ca
Nelly <Nelly.sosobrado@gmail.com>
2025-01-08 08:19:03 +0100

    Created 'CalculatorOperator', enums & Calculator.cs to define the calculator class with properties for calculations

---
+++ ClassLibrary/Models/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Models
+{
+    public class Calculator
+    {
+        public int Id { get; set; }
+        public double Operand1 { get; set; }
+        public double Operand2 { get; set; }
+        public CalculatorOperator Operator { get; set; }
+        public double Result { get; set; }
+        public DateTime CalculationDate { get; set; } = DateTime.Now;
+    }
+}
