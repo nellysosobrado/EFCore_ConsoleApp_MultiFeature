13956b73abf1a88d5ea5a6d87e7a0da30b36861a
Nelly <Nelly.sosobrado@gmail.com>
2025-01-09 17:16:50 +0100

    Installed Fluent Validation

---
+++ CalculatorApp/Validators/CalculatorValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using ClassLibrary.Models;
+using ClassLibrary.Enums;
+
+namespace CalculatorApp.Validators;
+
+public class CalculatorValidator : AbstractValidator<Calculator>
+{
+    public CalculatorValidator()
+    {
+        RuleFor(x => x.Operand1)
+            .NotNull()
+            .WithMessage("First operand is required");
+
+        RuleFor(x => x.Operand2)
+            .NotNull()
+            .WithMessage("Second operand is required")
+            .Must((calculator, operand2) =>
+                calculator.Operator != CalculatorOperator.Divide || operand2 != 0)
+            .WithMessage("Cannot divide by zero");
+
+        RuleFor(x => x.Operator)
+            .IsInEnum()
+            .WithMessage("Invalid operator");
+
+        RuleFor(x => x.CalculationDate)
+            .NotNull()
+            .WithMessage("Calculation date is required")
+            .LessThanOrEqualTo(DateTime.Now)
+            .WithMessage("Calculation date cannot be in the future");
+    }
+}
