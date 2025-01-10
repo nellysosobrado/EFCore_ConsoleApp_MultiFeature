4b72647ef0050eefe757af3185108aebf1013e6a
Nelly <Nelly.sosobrado@gmail.com>
2025-01-10 11:57:02 +0100

    Left main. Added back App.cs, Menu,cs, Program,cs.

---
+++ StartUp/Menu.cs
@@ -0,0 +1,57 @@
+﻿using Autofac;
+using CalculatorApp.Controllers;
+using Spectre.Console;
+
+namespace StartUp;
+
+public class Menu
+{
+    private readonly IContainer _container;
+
+    public Menu(IContainer container)
+    {
+        _container = container;
+    }
+
+    public void Show()
+    {
+        Console.Clear();
+
+        var option = AnsiConsole.Prompt(
+            new SelectionPrompt<MenuOptions>()
+                .Title("[green]Choose an option:[/]")
+                .UseConverter(option => option.GetDescription())
+                .AddChoices(Enum.GetValues<MenuOptions>()));
+
+        HandleMenuOption(option);
+    }
+
+    private void HandleMenuOption(MenuOptions option)
+    {
+        switch (option)
+        {
+            case MenuOptions.StartCalculator:
+                StartCalculator();
+                break;
+
+            case MenuOptions.StartShapes:
+                AnsiConsole.MarkupLine("[yellow]Shapes logic not implemented yet.[/]");
+                Console.ReadKey();
+                break;
+
+            case MenuOptions.Exit:
+                AnsiConsole.MarkupLine("[red]Exiting the application...[/]");
+                Environment.Exit(0);
+                break;
+        }
+    }
+
+    private void StartCalculator()
+    {
+        using (var scope = _container.BeginLifetimeScope())
+        {
+            var calculatorController = scope.Resolve<CalculatorController>();
+            calculatorController.Start();
+        }
+    }
+}
--- StartUp/Program.cs
+++ StartUp/Program.cs
@@ -1,57 +1,10 @@
-﻿using Autofac;
-using Spectre.Console;
-using StartUp;
-using CalculatorApp.Controllers;
-using Startup;
-
-namespace StartUp;
+﻿namespace StartUp;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        var container = ContainerConfig.Configure();
-
-        while (true)
-        {
-            ShowMenu(container);
-        }
-    }
-
-    private static void ShowMenu(IContainer container)
-    {
-        Console.Clear();
-
-        var option = AnsiConsole.Prompt(
-            new SelectionPrompt<MenuOptions>()
-                .Title("[green]Choose an option:[/]")
-                .UseConverter(option => option.GetDescription())
-                .AddChoices(Enum.GetValues<MenuOptions>()));
-
-        switch (option)
-        {
-            case MenuOptions.StartCalculator:
-                StartCalculator(container);
-                break;
-
-            case MenuOptions.StartShapes:
-                AnsiConsole.MarkupLine("[yellow]Shapes logic not implemented yet.[/]");
-                Console.ReadKey();
-                break;
-
-            case MenuOptions.Exit:
-                AnsiConsole.MarkupLine("[red]Exiting the application...[/]");
-                Environment.Exit(0);
-                break;
-        }
-    }
-
-    private static void StartCalculator(IContainer container)
-    {
-        using (var scope = container.BeginLifetimeScope())
-        {
-            var calculatorController = scope.Resolve<CalculatorController>();
-            calculatorController.Start();
-        }
+        var app = new App();
+        app.Run();
     }
 }
