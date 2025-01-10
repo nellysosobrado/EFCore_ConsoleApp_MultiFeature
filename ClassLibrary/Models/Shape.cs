e027bdc2ccfb9e175b32a1055279a1c66730212a
Nelly <Nelly.sosobrado@gmail.com>
2025-01-10 23:10:25 +0100

    Added shapetype enums.
    Created a new class 'Shape.cs'

---
+++ ClassLibrary/Models/Shape.cs
@@ -0,0 +1,14 @@
+﻿using ClassLibrary.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClassLibrary.Models;
+
+public class Shape
+{
+    public int Id { get; set; }
+    public ShapeType ShapeType { get; set; }
+    public double Area { get; set; }
+    public double Perimeter { get; set; }
+    public Dictionary<string, double> Parameters { get; set; } = new();
+    public DateTime CalculationDate { get; set; }
+}
