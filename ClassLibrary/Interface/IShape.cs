01e3ead5c843f894ccf6d3bb606ed6c9af41e6e0
Nelly <Nelly.sosobrado@gmail.com>
2025-01-11 08:11:38 +0100

    Created Shapes.cs Parallelogram, Rectangle, Rhombus, Triangle classes
    - new folder called ShapeAppRepository in the project.
    
    -new interface called IShape that has methods to calculate area and perimeter, and to get and set shape parameters.
    
    Implemented IShape interface in four different shape classes:
    
    -Parallelogram: Has base, height, and side.
    
    -Rectangle: Has width and height.
    
    Rhombus: Has side and height.
    
    Triangle: Has three sides and height.

---
+++ ClassLibrary/Interface/IShape.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassLibrary.Services.Shapes;
+
+public interface IShape
+{
+    double CalculateArea();
+    double CalculatePerimeter();
+    Dictionary<string, double> GetParameters();
+    void SetParameters(Dictionary<string, double> parameters);
+}
---
+++ ClassLibrary/Services/Shapes/Rhombus.cs
@@ -0,0 +1,25 @@
+﻿namespace ClassLibrary.Services.Shapes;
+
+public class Rhombus : IShape
+{
+    private double _side;
+    private double _height;
+
+    public Dictionary<string, double> GetParameters()
+    {
+        return new Dictionary<string, double>
+        {
+            { "Side", _side },
+            { "Height", _height }
+        };
+    }
+
+    public void SetParameters(Dictionary<string, double> parameters)
+    {
+        _side = parameters["Side"];
+        _height = parameters["Height"];
+    }
+
+    public double CalculateArea() => _side * _height;
+    public double CalculatePerimeter() => 4 * _side;
+}
